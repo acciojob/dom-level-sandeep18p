4e5c279268558169fc86fe3ed1cdfdfa54390818
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-20 21:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getLevel(level){
+  	 var ele = document.getElementById(level);
+	var count = 0;
+     while(ele!=null){
+		 count++;
+		 ele=ele.parentElement;
+	 }
+	return count;
+}
+var domL=getLevel('level');
+alert("The level of the element is: " + domL);
